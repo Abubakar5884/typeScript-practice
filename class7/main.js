5dfaa41e472adca3876deb4204fcedce116572c5
Abubakar siddique <156295005+Abubakar5884@users.noreply.github.com>
2024-05-25 15:54:31 +0500

    ARRAYS

---
+++ class7/main.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// console.log(num.pop())
+// console.log(num)
+// console.log(num.push(10))
+// console.log(num)
+// console.log(num.shift())
+// console.log(num)
+// console.log(num.unshift(25))
+// console.log(num)
+console.log(num.slice(5));
+console.log(num);
+// console.log(num.splice(1,5,99))
+// console.log(num)
