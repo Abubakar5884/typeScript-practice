a0a3065e0c9971f94a61cf7421f03826b3d01ed4
Abubakar siddique <156295005+Abubakar5884@users.noreply.github.com>
2024-05-25 14:00:26 +0500

    class6

---
+++ class6/main.js
@@ -0,0 +1,19 @@
+"use strict";
+// arrow function
+Object.defineProperty(exports, "__esModule", { value: true });
+// let arwfunc = (val:number,val1:number) => 
+//     console.log(val,val1)
+// arwfunc(5,10) 
+// return
+// function checking (a:number,b:number):number {
+//     return a + b
+// }
+// checking(5,5)
+function checking(a, b) {
+    return a + b;
+}
+console.log(checking(5, 20));
+// let arwfunc = (a:number) => {
+//     return console.log(a)
+// }
+// arwfunc(15)
--- class6/main.ts
+++ class6/main.ts
@@ -1,1 +1,32 @@
+// arrow function
+
+// let arwfunc = (val:number,val1:number) => 
+//     console.log(val,val1)
+// arwfunc(5,10) 
+
+
+
+// return
+
+// function checking (a:number,b:number):number {
+//     return a + b
+// }
+
+// checking(5,5)
+
+
+
+function checking (a:number,b:number) {
+    return a + b
+}
+
+console.log(checking(5,20))
+
+
+// let arwfunc = (a:number) => {
+//     return console.log(a)
+// }
+
+// arwfunc(15)
+
 
