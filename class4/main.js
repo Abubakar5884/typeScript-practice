caee3e56e1695fb4e9fb1d88a2a0f1fa8041238b
Abubakar siddique <156295005+Abubakar5884@users.noreply.github.com>
2024-05-25 13:56:28 +0500

    class4

---
+++ class4/main.js
@@ -0,0 +1,34 @@
+"use strict";
+// IF-ELSE STATEMENT
+Object.defineProperty(exports, "__esModule", { value: true });
+// let mem = "muhammad";
+// if (mem === "muhammad") {
+//     console.log("you are welcome")
+// } else if (mem === "abubakar") {
+//     console.log("you are not welcome")
+// } else if (mem === "ahmed") {
+//     console.log("why are you here")
+// } else {
+//     console.log("your entry is not possible")
+// }
+// IF STATEMENT NESTED
+// // ELIGIBILITY FOR DRIVING LICENCE
+// let nationality = "pakistani";
+// let age = 20;
+// if (nationality  === "pakistani") {
+//     if (age >= 18) {
+//         console.log("you will get your licence soon")
+//     } else {
+//         console.log("you are underage")
+//     }
+// } else {
+//     console.log("nationality must be pakistani for licence")
+// }
+let user = "abubakar";
+let pin = 555;
+if (user === "abubakar" && pin === 555) {
+    console.log("welcome to your account");
+}
+else {
+    console.log("invalid user!");
+}
