2cf82c3502bb2ef6c908b79897f9385d54187ed8
Abubakar siddique <156295005+Abubakar5884@users.noreply.github.com>
2024-05-25 13:55:30 +0500

    class3

---
+++ class3/main.js
@@ -0,0 +1,55 @@
+"use strict";
+// OPERATORS
+Object.defineProperty(exports, "__esModule", { value: true });
+// ADDITION =>
+// let num1 = 5;
+// let num2 = 10;
+// console.log(`${num1 + num2} \n`);
+// let sum = num1 + num2;
+// console.log(`${sum}\n`);
+// SUBTRACTION =>
+// let num1 = 5;
+// let num2 = 10;
+// console.log(`${num1 - num2} \n`);
+// let sub = num1 - num2;
+// console.log(`${sub}\n`);
+// MULTIPLICATION =>
+// let num1 = 5;
+// let num2 = 10;
+// console.log(`${num1 * num2} \n`);
+// let mul = num1 * num2;
+// console.log(`${mul}\n`);
+// DIVISION =>
+// let num1 = 10;
+// let num2 = 5;
+// console.log(`${num1 / num2} \n`);
+// let dev = num1 / num2;
+// console.log(`${dev}\n`);
+// MODULOS =>
+// let num1 = 10;
+// let num2 = 3;
+// console.log(`${num1 % num2} \n`);
+// let mod = num1 % num2;
+// console.log(`${mod} \n`);
+// ASSIGNMENT OPERATORS =>
+// let a = 5;
+// console.log(a + a);
+// console.log(a += 10);
+// console.log(a -= 2);
+// COMPARISION OPERATORS =>
+// let n = 5;
+// let n1 = "abc";
+// console.log(n == 5); // true
+// console.log(n === 6); // false
+// console.log(n != 5); // false 
+// console.log(n < 10); // true
+// console.log(n > 9); // false 
+// console.log(n <= 4); // false 
+// console.log(n >= 2); // true
+// LOGICAL OPERATORS =>
+let logicalAnd = 5;
+console.log(logicalAnd >= 4 && logicalAnd === 5); // agr dono jagha true hoga to ans true dy ga or agr kisi ik gajha bhi false hua to ans bhi false hi dy ga 
+let logicalOr = 5;
+console.log(logicalOr >= 5 || logicalOr === 6); // agr ik statement true ho jati ha to true print kry ga or false print krny ka lia dono statement ko false hona pry ga
+let logicalNot = 5;
+console.log(logicalNot > 4); // in this condition it will print false because of the not operator
