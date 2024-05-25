ed40ea0248bdce66dc107cc1f2e004d09d1265a1
Abubakar siddique <156295005+Abubakar5884@users.noreply.github.com>
2024-05-25 13:54:26 +0500

    class2

---
+++ class2/main1.js
@@ -0,0 +1,18 @@
+"use strict";
+// console.log("Welcome to typescript\n");
+Object.defineProperty(exports, "__esModule", { value: true });
+// let name:string = "Abubakar Siddique";
+// let fName:string = "Kamran Hussain";
+// let ismarried:boolean = false
+// let age:number = 17;
+// console.log(name + "\n");
+// console.log(fName + "\n");
+// console.log(ismarried + "\n");
+// console.log(age + "\n");
+// TEMPLATE LITERALS
+let frstName = "Abuabakar ";
+let lstName = "Siddique";
+let hello = "Hello, " + frstName + lstName;
+console.log(hello + "\n");
+console.log(`${frstName} ${lstName} \n`);
+console.log(`${hello}`);
