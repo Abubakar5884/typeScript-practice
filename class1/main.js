4bbffad36cdea6947fa24df968a12fe12d027be9
Abubakar siddique <156295005+Abubakar5884@users.noreply.github.com>
2024-05-25 13:53:15 +0500

    Add files via upload

---
+++ class1/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("welcome to typescript");
+let name = "Abubakar Siddique";
+name = "Ahmed Raza";
+console.log(name);
--- class1/main.ts
+++ class1/main.ts
@@ -1,1 +1,6 @@
+console.log("welcome to typescript");
 
+let name = "Abubakar Siddique";
+name = "Ahmed Raza";
+
+console.log(name);
