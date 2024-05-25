4acca096e7b22efa19411f19ca512bcccef91ff9
Abubakar siddique <156295005+Abubakar5884@users.noreply.github.com>
2024-05-25 13:58:04 +0500

    class5

---
+++ class5/main.js
@@ -0,0 +1,15 @@
+"use strict";
+// FUNCTIONS 
+Object.defineProperty(exports, "__esModule", { value: true });
+// function abubakar (work:string){
+//     console.log("you didn't do your work")
+// }
+// abubakar()
+// function muhammad (dahi:number):void{
+//     console.log("HI")
+// }
+// muhammad(4)
+// ARROW/LAMBDA FUNCTION
+// let myfnc = (val:string,val1:string) => 
+//     console.log(val,val1)
+// console.log(myfnc("hi","hello"))
