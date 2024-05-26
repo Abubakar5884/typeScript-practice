d92e6cfd608ae2db9e8d81d0b7fb61321bdbbbc2
Abubakar siddique <156295005+Abubakar5884@users.noreply.github.com>
2024-05-26 09:12:50 +0500

    check cities.ts
    i turned my imagination into code while learning for loops from a yt video

---
+++ class8/cities.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+let cities = ["hyderabad", "karachi", "islamabad", "lahore", "nawabshah", "quetta", "peshawer", "larkana", "skardu", "rawalpindi", "multan"];
+for (let i = "islamabad";;) {
+    let answer = await inquirer.prompt([{
+            name: "city",
+            type: "input",
+            message: "what is the cleanest city in pakistan"
+        }]);
+    if (answer.city === "islamabad") {
+        console.log(`${answer.city} is the cleanest city.`);
+        break;
+    }
+    else
+        (console.log("You are wrong"));
+}
+// FIRST EVER VERSION OF THIS PROGRAM
+// import inquirer from "inquirer"
+// let cities = ["hyderabad", "karachi", "islamabad", "lahore", "nawabshah", "quetta", "peshawer", "larkana", "skardu", "rawalpindi", "multan"]
+// for (let i = 0; i <= 10; i++){
+// let answer = await inquirer.prompt([{
+//     name:"city",
+//     type:"input",
+//     message:"what is the cleanest city in pakistan"
+// }])
+// if (answer.city === "islamabad")
+//     if(answer.city === "islamabad"){
+//         console.log(`${answer.city} is the cleanest city.`)
+//     } else {
+//         break;
+//     } else (console.log("You are wrong"))
+// }
---
+++ class8/main.js
@@ -0,0 +1,25 @@
+// FOR LOOP
+// for (let i = 1; i <= 5; i++){
+//     console.log("hello world")
+// }
+// let cleanCities = ["Karachi", "Islamabad", "Hyderabad", "Lahore"]
+// for (let i = 0; i < 4; i++){
+//     if ("Hyderabad" === cleanCities[i]){
+//         console.log(`${cleanCities[i]} is the cleanest city.`)
+//         break;
+//     }   
+// }
+// let a = 10
+// let b = ""
+// for (let i = 0; i <= a; i++){
+//     for (let n = 0; n <= i; n++){
+//         b += "*"
+//     }
+//     b += "\n"  
+// } 
+// console.log(b);
+let number = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} * ${i} = ${number * i}`);
+}
+export {};
